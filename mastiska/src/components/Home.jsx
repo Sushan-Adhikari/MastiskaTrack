bc7dd3b88b51971a9842cc0afe8c799ca0170e25
phirojshah <aditya8877123@gmail.com>
2023-09-03 14:38:48 +0545

    Many section updated

---
+++ mastiska/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      "Home"
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
